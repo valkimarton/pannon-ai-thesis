d56ab2ba6148675edf11b9b1f45cd8c6d6d5d19b
Marton Valki <valkimarton@gmail.com>
2024-05-04 21:20:16 +0200

    Read input data

---
+++ main/data_prep.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import os
+
+DATA_FOLDER = './data/'
+BIAX_TENSION_FOLDER = 'biax_tension/'
+PLANAR_COMPRESSION_FOLDER = 'planar_compression/'
+PLANAR_TENSION_FOLDER = 'planar_tension/'
+SIMPLE_SHEAR_FOLDER = 'simple_shear/'
+UNIAX_COMPRESSION_FOLDER = 'uniax_compression/'
+UNIAX_TENSION_FOLDER = 'uniax_tension/'
+
+NEO_HOOKEAN_FOLDER = 'neohookean/'
+MOONEY_RIVLIN_FOLDER = 'mooneyrivlin2/'
+OGDEN_FOLDER = 'ogden/'
+
+# read biax_tension data into dataFrames
+# - neo-hookean
+def read_biax_tension_neo_hookean() -> dict:
+    biax_tension_neo_hookean = {}
+    data_folder = './data/biax_tension/neohookean'
+    for file in os.listdir(data_folder):
+        if file.endswith('.txt'):
+            # read data. values are separated by simple space
+            data = pd.read_csv(data_folder + '/' + file, sep=' ', header=None)
+            data.columns = ['X', 'Y']
+            data_point_number = file[-7:-4]
+            biax_tension_neo_hookean[data_point_number] = data
+            return biax_tension_neo_hookean
+# - mooney-rivlin
+# - ogden
+
+def read_inputs(tension_folder: str, mat_model_folder: str) -> dict:
+    inputs = {}
+    data_folder = DATA_FOLDER + tension_folder + mat_model_folder
+    for file in os.listdir(data_folder):
+        if file.endswith('.txt'):
+            data = pd.read_csv(data_folder + '/' + file, sep='  ', header=None, engine='python')
+            # remove rows where all values are 0
+            data = data[(data.T != 0).any()]
+            data.columns = ['stress', 'strain']   #TODO
+            data_point_number = file[:-4]
+            inputs[data_point_number] = data
+    return inputs
+
+
+
+
+
---
+++ main/main.py
@@ -0,0 +1,29 @@
+import data_prep
+
+if __name__ == '__main__':
+    inputs_biax_nh = data_prep.read_inputs(data_prep.BIAX_TENSION_FOLDER, data_prep.NEO_HOOKEAN_FOLDER)
+    inputs_biax_mr = data_prep.read_inputs(data_prep.BIAX_TENSION_FOLDER, data_prep.MOONEY_RIVLIN_FOLDER)
+    inputs_biax_og = data_prep.read_inputs(data_prep.BIAX_TENSION_FOLDER, data_prep.OGDEN_FOLDER)
+
+    inputs_planarc_nh = data_prep.read_inputs(data_prep.PLANAR_COMPRESSION_FOLDER, data_prep.NEO_HOOKEAN_FOLDER)
+    inputs_planarc_mr = data_prep.read_inputs(data_prep.PLANAR_COMPRESSION_FOLDER, data_prep.MOONEY_RIVLIN_FOLDER)
+    inputs_planarc_og = data_prep.read_inputs(data_prep.PLANAR_COMPRESSION_FOLDER, data_prep.OGDEN_FOLDER)
+
+    inputs_planart_nh = data_prep.read_inputs(data_prep.PLANAR_TENSION_FOLDER, data_prep.NEO_HOOKEAN_FOLDER)
+    inputs_planart_mr = data_prep.read_inputs(data_prep.PLANAR_TENSION_FOLDER, data_prep.MOONEY_RIVLIN_FOLDER)
+    inputs_planart_og = data_prep.read_inputs(data_prep.PLANAR_TENSION_FOLDER, data_prep.OGDEN_FOLDER)
+
+    inputs_shear_nh = data_prep.read_inputs(data_prep.SIMPLE_SHEAR_FOLDER, data_prep.NEO_HOOKEAN_FOLDER)
+    inputs_shear_mr = data_prep.read_inputs(data_prep.SIMPLE_SHEAR_FOLDER, data_prep.MOONEY_RIVLIN_FOLDER)
+    inputs_shear_og = data_prep.read_inputs(data_prep.SIMPLE_SHEAR_FOLDER, data_prep.OGDEN_FOLDER)
+    
+    inputs_uniax_nh = data_prep.read_inputs(data_prep.UNIAX_COMPRESSION_FOLDER, data_prep.NEO_HOOKEAN_FOLDER)
+    inputs_uniax_mr = data_prep.read_inputs(data_prep.UNIAX_COMPRESSION_FOLDER, data_prep.MOONEY_RIVLIN_FOLDER)
+    inputs_uniax_og = data_prep.read_inputs(data_prep.UNIAX_COMPRESSION_FOLDER, data_prep.OGDEN_FOLDER)
+
+    # TODO: handle uniax_tension's 3 columns
+    # inputs_uniaxc_nh = data_prep.read_inputs(data_prep.UNIAX_TENSION_FOLDER, data_prep.NEO_HOOKEAN_FOLDER)
+    # inputs_uniaxc_mr = data_prep.read_inputs(data_prep.UNIAX_TENSION_FOLDER, data_prep.MOONEY_RIVLIN_FOLDER)
+    # inputs_uniaxc_og = data_prep.read_inputs(data_prep.UNIAX_TENSION_FOLDER, data_prep.OGDEN_FOLDER)
+
+    
