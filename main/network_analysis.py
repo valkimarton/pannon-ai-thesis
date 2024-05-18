e87d493bd94f9a1f9047e3890f44fa219b083da6
Marton Valki <valkimarton@gmail.com>
2024-05-18 21:28:37 +0200

    Create resampled data for dynamic time warping

--- main/network_analysis.py
+++ main/network_analysis.py
@@ -1,3 +1,4 @@
+import os
 import data_prep
 import pandas as pd
 import numpy as np
@@ -5,6 +6,19 @@ from dtaidistance import dtw
 from dtaidistance import dtw_visualisation as dtwvis
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
+
+RESAMPLED_DATA_FOLDER = './resampled_data/'
+
+BIAX_TENSION_FOLDER = 'biax_tension/'
+PLANAR_COMPRESSION_FOLDER = 'planar_compression/'
+PLANAR_TENSION_FOLDER = 'planar_tension/'
+SIMPLE_SHEAR_FOLDER = 'simple_shear/'
+UNIAX_COMPRESSION_FOLDER = 'uniax_compression/'
+UNIAX_TENSION_FOLDER = 'uniax_tension/'
+
+NEO_HOOKEAN_FOLDER = 'neohookean/'
+MOONEY_RIVLIN_FOLDER = 'mooneyrivlin2/'
+OGDEN_FOLDER = 'ogden/'
 
 # function to calculate similarity with dynamic time warping
 def dtw_similarity(data1: pd.DataFrame, data2: pd.DataFrame) -> float:
@@ -20,7 +34,7 @@ def dtw_similarity(data1: pd.DataFrame, data2: pd.DataFrame) -> float:
     
     return dtw[n][m]
 
-def max_first_strain_biax_tension(inputs_nh: dict[str, pd.DataFrame], inputs_mr: dict[str, pd.DataFrame], inputs_og: dict[str, pd.DataFrame]) -> float:
+def get_max_first_strain_biax_tension(inputs_nh: dict[str, pd.DataFrame], inputs_mr: dict[str, pd.DataFrame], inputs_og: dict[str, pd.DataFrame]) -> float:
     max_first_strain = 0
     for inputs in [inputs_nh, inputs_mr, inputs_og]:
         for key, data in inputs.items():
@@ -66,15 +80,78 @@ def resample_single_biax_tension(input: pd.DataFrame, resampled_strain: np.ndarr
 def resample_biax_tension(inputs: dict[str, pd.DataFrame], resampled_strain: np.ndarray) -> dict[str, pd.DataFrame]:
     resampled_biax_tension_data: dict[str, pd.DataFrame] = {}
     for key, data in inputs.items():
-        interp_func = interp1d(data['strain'], data['stress'], kind='linear', fill_value=0, bounds_error=False)
-        resampled_stress = interp_func(resampled_strain)
-        resampled_biax_tension_data[key] = pd.DataFrame({'strain': resampled_strain, 'stress': resampled_stress})
+        resampled_biax_tension_data[key] = resample_single_biax_tension(data, resampled_strain)
+    return resampled_biax_tension_data
+    
 
+def save_resampled_data(resampled_nh: dict[str, pd.DataFrame], 
+                        resampled_mr: dict[str, pd.DataFrame], 
+                        resampled_og: dict[str, pd.DataFrame],
+                        stress_type_folder: str):
+    stress_type_folder_full = RESAMPLED_DATA_FOLDER + stress_type_folder
+    for key, data in resampled_nh.items():
+        folder = stress_type_folder_full + NEO_HOOKEAN_FOLDER
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        data.to_csv(folder + key + '.txt', sep=',', index=False)
+    for key, data in resampled_mr.items():
+        folder = stress_type_folder_full + MOONEY_RIVLIN_FOLDER
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        data.to_csv(folder + key + '.txt', sep=' ', index=False)
+    for key, data in resampled_og.items():
+        folder = stress_type_folder_full + OGDEN_FOLDER
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        data.to_csv(folder + key + '.txt', sep=' ', index=False)
 
-if __name__ == '__main__':
+def create_resampled_inputs():
     inputs_biax_nh: dict[str, pd.DataFrame] = data_prep.read_inputs(data_prep.BIAX_TENSION_FOLDER, data_prep.NEO_HOOKEAN_FOLDER)
     inputs_biax_mr: dict[str, pd.DataFrame] = data_prep.read_inputs(data_prep.BIAX_TENSION_FOLDER, data_prep.MOONEY_RIVLIN_FOLDER)
     inputs_biax_og: dict[str, pd.DataFrame] = data_prep.read_inputs(data_prep.BIAX_TENSION_FOLDER, data_prep.OGDEN_FOLDER)
+
+    min_strain_diff_biax_tension, max_strain_diff_biax_tension = find_min_and_max_strain_interval_for_stress_type(inputs_biax_nh, inputs_biax_mr, inputs_biax_og)
+    strain_interval_biax_tension: float = min_strain_diff_biax_tension / 10
+    strain_diff_90th_percentile = determine_max_strain_diff_90th_percentile(inputs_biax_nh, inputs_biax_mr, inputs_biax_og)
+    sample_size_biax_tension: int = int(strain_diff_90th_percentile / strain_interval_biax_tension)
+    max_first_strain_biax_tension: float = get_max_first_strain_biax_tension(inputs_biax_nh, inputs_biax_mr, inputs_biax_og)
+
+    # resample every stress data to strain_interval and sample_size_biax_tension then fill missing values with 0
+    resampled_strain = np.linspace(0, strain_interval_biax_tension * sample_size_biax_tension, sample_size_biax_tension)
+    resampled_biax_nh: dict[str, pd.DataFrame] = resample_biax_tension(inputs_biax_nh, resampled_strain)
+    resampled_biax_mr: dict[str, pd.DataFrame] = resample_biax_tension(inputs_biax_mr, resampled_strain)
+    resampled_biax_og: dict[str, pd.DataFrame] = resample_biax_tension(inputs_biax_og, resampled_strain)
+
+    # save resampled data
+    save_resampled_data(resampled_biax_nh, resampled_biax_mr, resampled_biax_og, BIAX_TENSION_FOLDER)
+
+def read_resampled_data(data_folder: str) -> dict[str, pd.DataFrame]:
+    resampled_data: dict[str, pd.DataFrame] = {}
+    for file in os.listdir(data_folder):
+        if file.endswith('.txt'):
+            data = pd.read_csv(data_folder + file, sep=',')
+            data.columns = ['strain', 'stress']
+            data_point_number = file[:-4]
+            resampled_data[data_point_number] = data
+    return resampled_data
+
+
+
+if __name__ == '__main__':
+    #create_resampled_inputs()
+
+    resampled_biax_nh: dict[str, pd.DataFrame] = read_resampled_data(RESAMPLED_DATA_FOLDER + BIAX_TENSION_FOLDER + NEO_HOOKEAN_FOLDER)
+    resampled_biax_mr: dict[str, pd.DataFrame] = read_resampled_data(RESAMPLED_DATA_FOLDER + BIAX_TENSION_FOLDER + MOONEY_RIVLIN_FOLDER)
+    resampled_biax_og: dict[str, pd.DataFrame] = read_resampled_data(RESAMPLED_DATA_FOLDER + BIAX_TENSION_FOLDER + OGDEN_FOLDER)
+
+    # calculate similarity with dynamic time warping
+
+    # calculate dynamic time warping similarity between two inputs
+
+
+
+
+
 
     # TODO:
     # KNN gráffal gráf a mintákból -> megnézni hogy kialakul-e a három klaszter
@@ -82,55 +159,8 @@ if __name__ == '__main__':
     # -	Dinamikus idősorvetemítéssel
     # Interpolálni a görbéket úgy hogy azonos strain értékekkel dolgozzak: 
     # - Kitölteni a rövideket 0-kkal és ezután mehet a Dinamikus idősorvetemítés
-
-    # find min and max strain interval for each stress type
-    min_strain_diff_biax_tension, max_strain_diff_biax_tension = find_min_and_max_strain_interval_for_stress_type(inputs_biax_nh, inputs_biax_mr, inputs_biax_og)
-    # find optimal strain interval before resampling data
-    strain_interval_biax_tension: float = min_strain_diff_biax_tension / 10
-    # determine number of points for resampling - round down to nearest integer max / interval
-
-
-    # determine 90th percentile of max_strain_diff
-    strain_diff_90th_percentile = determine_max_strain_diff_90th_percentile(inputs_biax_nh, inputs_biax_mr, inputs_biax_og)
-
-    sample_size_biax_tension: int = int(strain_diff_90th_percentile / strain_interval_biax_tension)
-    max_first_strain_biax_tension: float = max_first_strain_biax_tension(inputs_biax_nh, inputs_biax_mr, inputs_biax_og)
-
-    # resample every stress data to strain_interval and sample_size_biax_tension then fill missing values with 0
-    resampled_strain = np.linspace(
-        max_first_strain_biax_tension, 
-        max_first_strain_biax_tension + strain_interval_biax_tension * sample_size_biax_tension, 
-        sample_size_biax_tension)
-    # resampled_biax_nh: dict[str, pd.DataFrame] = resample_biax_tension(inputs_biax_nh, resampled_strain)
-    # resampled_biax_mr: dict[str, pd.DataFrame] = resample_biax_tension(inputs_biax_mr, resampled_strain)
-    # resampled_biax_og: dict[str, pd.DataFrame] = resample_biax_tension(inputs_biax_og, resampled_strain)
-
-    bt_nh_0: pd.DataFrame = inputs_biax_nh['biax_tension_neohookean_dp0']
-    bt_mr_0: pd.DataFrame = inputs_biax_mr['biax_tension_mooneyrivlin2_dp0']
-    bt_og_56: pd.DataFrame = inputs_biax_og['biax_tension_ogden_dp56']
-
-    resampled_bt_nh_0: pd.DataFrame = resample_single_biax_tension(bt_nh_0, resampled_strain)
-    resampled_bt_mr_0: pd.DataFrame = resample_single_biax_tension(bt_mr_0, resampled_strain)
-    resampled_bt_og_56: pd.DataFrame = resample_single_biax_tension(bt_og_56, resampled_strain)
-
-    # plot original and resampled data
-    plt.plot(bt_nh_0['strain'], bt_nh_0['stress'], label='neohookean dp0')
-    plt.plot(resampled_bt_nh_0['strain'], resampled_bt_nh_0['stress'], label='resampled neohookean dp0')
-    plt.legend()
-    plt.show()
-
-    plt.plot(bt_mr_0['strain'], bt_mr_0['stress'], label='mooneyrivlin2 dp0')
-    plt.plot(resampled_bt_mr_0['strain'], resampled_bt_mr_0['stress'], label='resampled mooneyrivlin2 dp0')
-    plt.legend()
-    plt.show()
-
-    plt.plot(bt_og_56['strain'], bt_og_56['stress'], label='ogden dp56')
-    plt.plot(resampled_bt_og_56['strain'], resampled_bt_og_56['stress'], label='resampled ogden dp56')
-    plt.legend()
-    plt.show()
-
-
     
+
     # inputs_planarc_nh: dict[str, pd.DataFrame] = data_prep.read_inputs(data_prep.PLANAR_COMPRESSION_FOLDER, data_prep.NEO_HOOKEAN_FOLDER)
     # inputs_planarc_mr: dict[str, pd.DataFrame] = data_prep.read_inputs(data_prep.PLANAR_COMPRESSION_FOLDER, data_prep.MOONEY_RIVLIN_FOLDER)
     # inputs_planarc_og: dict[str, pd.DataFrame] = data_prep.read_inputs(data_prep.PLANAR_COMPRESSION_FOLDER, data_prep.OGDEN_FOLDER)
